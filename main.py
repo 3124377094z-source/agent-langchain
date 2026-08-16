95b7025ca5a6507cb2b5f0e77ab86c051b60190b
3124377094z-source <3124377094z@gmail.com>
2026-08-17 00:22:58 +0800

    更新

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import  FastAPI
-
-from db.db_config import lifespan
+from contextlib import asynccontextmanager
+from db.db_config import create_tables,async_engine
+from db.db_redis import redis_connect
 from routers import agent_router,user
-app = FastAPI(lifespan=lifespan)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # 启动时执行建表
+    await create_tables()
+    app.state.redis = await redis_connect()
+    print("redis启动")
+    yield
+    # 关闭时释放连接池
+    await async_engine.dispose()
+    await app.state.redis.close()
+app = FastAPI(lifespan=lifespan,debug=True)
 app.include_router(agent_router.router)
 app.include_router(user.router)
